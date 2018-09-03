6f75c61a134bff4b2c5ee7cf0e62ea6b686ace28
float <zexiplus@outlook.com>
2018-09-03 11:07:04 +0800

    同时支持 windows 系统和 类unix系统

--- config.js
+++ config.js
@@ -1,7 +1,16 @@
 const path = require('path')
+const os = require('os')
+
+let url
+if (os.platform() === 'win32') {
+    url = 'file:///' + path.resolve(__dirname, './demo/index.html').replace(/\\/g, '/')
+} else {
+    url = 'file://' + path.resolve(__dirname, './demo/index.html')
+}
+
 const config = {
     path: path.resolve(__dirname, './demo'),
-    url: 'file:///' + path.resolve(__dirname, './demo/index.html').replace(/\\/g, '/'),
+    url: url,
     deep: true
 }
 module.exports = config
